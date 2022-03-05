2b8d9df3427062656d6240c9ec20ac2a664a196d
NhatBi <nhatbien99@gmail.com>
2022-03-05 15:52:55 +0700

    Hoan Thanh

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const express = require("express");
-const cors =     require("cors");
-const app  = express();
+const express = require('express')
+const cors = require('cors')
 
-app.use(cors());
+const setupContactRoutes = require('./app/routes/contact.routes')
+const { BadRequestError, errorHandler } = require('./app/errors')
 
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.get("/",(req, res)  =>{
-res.json({message: "well come"})
+app.use(cors())
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    // res.json({ message: "Welcome to contact book application." })
+    res.send('Welcome to homepage!')
 })
-module.exports = app;
+
+setupContactRoutes(app)
+
+app.use((req, res, next) => {
+    next(new BadRequestError(404, 'Resource not found'))
+})  
+
+app.use((err, req, res, next) => {
+    errorHandler.handleError(err, res)
+})
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const app = require("./app")
-const config = require("./app/config")
-const PORT  = config.app.port
+const app = require('./app')
+const config = require('./app/config')
+const mongoose = require('mongoose')
 
-app.listen(PORT,() => {
-    console.log(`Server running port: ${PORT}`)
+mongoose.connect(config.db.uri)
+    .then(() => {
+        console.log('Connected to the database!')
+    })
+    .catch((error) => {
+        console.log('Cannot connect to the database!', error)
+        process.exit()
+    })
+
+const PORT = config.app.port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`)
 })
